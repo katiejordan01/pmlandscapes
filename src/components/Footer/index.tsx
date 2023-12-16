dd23299fa19ef34d889ab0f613e97d33a8730c47
katiejordan01 <kjordan62@gatech.edu>
2023-12-16 01:00:03 -0500

    Footer resize

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -41,8 +41,8 @@ const Footer = ({ t }: { t: TFunction }) => {
     <>
       <FooterSection>
         <Container>
-          <Row justify="space-around">
-            <Col lg={8} md={8} sm={12} xs={24}>
+          <Row justify="space-around" gutter={[16, 16]}>
+            <Col lg={8} md={8} sm={40} xs={60} style={{marginLeft: "0px", marginRight: "0px" }}>
               <Language>{t("Contact")}</Language>
               <Para>
                 {t(`Do you have any questions? Feel free to reach out.`)}
@@ -51,7 +51,7 @@ const Footer = ({ t }: { t: TFunction }) => {
                 <Chat>{t(`Let's Chat`)}</Chat>
               </a>
             </Col>
-            <Col lg={8} md={8} sm={12} xs={24}>
+            <Col lg={8} md={8} sm={40} xs={60} style={{marginLeft: "0px", marginRight: "0px" }}>
               <Language>{t("Locations")}</Language>
               <LocationContainer>
                 <LocationItem>Snellville, GA</LocationItem>
@@ -59,26 +59,9 @@ const Footer = ({ t }: { t: TFunction }) => {
                 <LocationItem>Dawsonville, GA</LocationItem>
               </LocationContainer>
             </Col>
-            <Col lg={8} md={8} sm={12} xs={24}>
-            </Col>
           </Row>
         </Container>
       </FooterSection>
-      <Extra>
-        <Container border={true}>
-          <Row justify="space-around">
-            <Col lg={8} md={8} sm={12} xs={24}>
-              {/* Content for the first column */}
-            </Col>
-            <Col lg={8} md={8} sm={12} xs={24}>
-              {/* Content for the second column */}
-            </Col>
-            <Col lg={8} md={8} sm={12} xs={24}>
-              {/* Content for the third column */}
-            </Col>
-          </Row>
-        </Container>
-      </Extra>
       <Extra>
       <Container border={true}>
           <Row
