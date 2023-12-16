d5469ffd29ec66bbf28320ddffde19fc2b60806e
katiejordan01 <kjordan62@gatech.edu>
2023-12-15 21:11:51 -0500

    Error

--- src/components/MiddleBlock/styles.tsx
+++ src/components/MiddleBlock/styles.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Row } from "antd";
-import { SvgIcon } from "../../common/SvgIcon";
 
 export const MiddleBlockSection = styled("section")`
   position: relative;
